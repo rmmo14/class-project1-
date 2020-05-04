532c6210855d87e5c7a39b9913606d510b32d6a1
Rene Montoya <rmontoya.we@gmail.com>
2020-05-03 23:21:31 -0700

    updated notes

--- jscmd/get-interest.js
+++ jscmd/get-interest.js
@@ -1,14 +1,14 @@
 var response = prompt ('Are you a parent, student, or educator?');
 var message;
 
-if (message === 'parent') {
+if (response === 'parent') {
     message = 'Check out Normalize Mistakes';
-} else if (message === 'student') {
+} else if (response === 'student') {
     message = 'Be sure to read each section';
-} else if (message === 'educator') {
+} else if (response === 'educator') {
     message = 'Any tips you would suggest?';
 } else {
     message = 'Check out the page! :)';
 }
 
-document.write('<h2>'+ message + '</h2>'); 
+document.write('<h3>'+ message + '</h3>'); 

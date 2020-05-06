6c3cce0aa5d0a3afff2ac8f7fb2842bccb369dab
Rene Montoya <rmontoya.we@gmail.com>
2020-05-05 18:28:39 -0700

    adding another dynamic

--- jscmd/app.js
+++ jscmd/app.js
@@ -31,3 +31,20 @@ function mathInterest(){
 
     return document.write('<h3>' + rep + '</h3>');
 }
+
+function myFunction() {
+    confirm("assume by contradiction the sqrt2 is rational, then sqrt2 can be expressed as p:q where p,q are integers, q neq 0, and gcd(p,q)=1. Now with sqrt2=p:q, squaring both sides will give 2=(p:q)^(2), moving over the q gives 2q^(2)=p^(2). This implies p^(2) is even and thus p is even of the form p=2k, where k is an integer. By substitution then 2q^(2)=p^(2)=(2k)^(2) which gives q^(2)=2k^(2). This implies q^(2) is even and thus q is even. BUT, this contradicts the fact that gcd(p,q)=1, therefore sqrt2 is not rational. QED.*");
+}
+
+function mathQuiz(){
+    var resp = prompt ('Would you like to try a math problem?')
+    var ans;
+
+    if (resp === 'yes' || resp === 'Yes' || resp === 'y' || resp === 'sure'){
+        ans = 'Prove the irrationality of root 2. (See answer on page to verify)';
+    } else {
+        resp = 'The answer to the math problem is on the page if you were still curious';
+    }
+
+    return document.write('<h4>' + ans + '</h4>');
+}

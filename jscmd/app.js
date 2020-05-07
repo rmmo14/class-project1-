c4b1a07f90900a5698516ca4dc8254cdf1f0c2a8
Rene Montoya <rmontoya.we@gmail.com>
2020-05-06 20:17:43 -0700

    troubleshoot looping

--- jscmd/app.js
+++ jscmd/app.js
@@ -1,29 +1,29 @@
 function getInterest() {
-    var response = prompt ('Are you a parent, student, or educator?');
+    var response = prompt('Are you a parent, student, or educator?');
     var message;
 
     if (response === 'parent') {
-    message = 'Check out Normalize Mistakes';
+        message = 'Check out Normalize Mistakes';
     } else if (response === 'student') {
-    message = 'Be sure to read each section';
+        message = 'Be sure to read each section';
     } else if (response === 'educator') {
-    message = 'Any tips you would suggest?';
+        message = 'Any tips you would suggest?';
     } else {
-    message = 'Check out the page! :)';
+        message = 'Check out the page! :)';
     }
 
-    return document.write('<h3>'+ message + '</h3>'); 
+    return document.write('<h3>' + message + '</h3>');
 }
 
-function mathInterest(){
-    var answer = prompt ('Which of these was your favorite: algebra, geometry, algebra 2?');
+function mathInterest() {
+    var answer = prompt('Which of these was your favorite: algebra, geometry, algebra 2?');
     var rep;
 
-    if (answer === 'algebra'){
+    if (answer === 'algebra') {
         rep = 'Mine too!';
-    } else if (answer === 'geometry'){
+    } else if (answer === 'geometry') {
         rep = 'Gee I\'m A Tree';
-    } else if (answer === 'algebra 2'){
+    } else if (answer === 'algebra 2') {
         rep = 'Oof that one was craaaazy fun!';
     } else {
         rep = 'Math is Fun!!!';
@@ -33,47 +33,67 @@ function mathInterest(){
 }
 
 
-function assessMath(){
+function assessMath() {
     var question = prompt('Quiz in which area: algebra, geometry, algebra 2?');
+    var attemptsAllowed = 2;
+    var attempted = 0;
     var pregunta;
     var respuesta;
 
-    if (question === 'algebra'){
+    if (question === 'algebra') {
         pregunta = prompt('Which is the slope in the equation 2x-2y=4?');
-        while (pregunta != 1){
-        pregunta = prompt('Try again. Which is the slope in the equation 2x-2y=4?');
+        while (pregunta != 1 && attempted !== attemptsAllowed) {
+            pregunta = prompt('Try again. Which is the slope in the equation 2x-2y=4?');
+            attempted++;
         }
-        respuesta = 'Correct!';
-    } else if (question === 'geometry'){
-        pregunta = prompt('What is the area of a 2ft by 18in window? Express in square ft?')
-        while (pregunta != 3){
-        pregunta = prompt ('Try again. What is the area of a 2ft by 18in window? Express in square ft?')
+        if (pregunta === '1') {
+            respuesta = 'Correct!';
+        } else if (attempted === attemptsAllowed) {
+            respuesta = 'Sorry!';
         }
-        respuesta = 'Correct!';
-    } else if (question === 'algebra 2'){
-        pregunta = prompt('What is ln (1) equal to?')
-        while (pregunta != 0){
-            pregunta = prompt('Try again. What is ln (1) equal to?')
+    } else if (question === 'geometry') {
+        pregunta = prompt('What is the area of a 2ft by 18in window? Express in square ft?');
+        while (pregunta != 3 && attempted !== attemptsAllowed) {
+            pregunta = prompt('Try again. What is the area of a 2ft by 18in window? Express in square ft?');
+            attempted++;
         }
-        respuesta = 'Correct!';
+        if (pregunta === '3') {
+            respuesta = 'Correct!';
+        } else if (attempted === attemptsAllowed) {
+            respuesta = 'Sorry!';
+        }
+    } else if (question === 'algebra 2') {
+        pregunta = prompt('What is ln (1) equal to?');
+        while (pregunta != '0' && attempted !== attemptsAllowed) {
+            pregunta = prompt('Try again. What is ln (1) equal to?');
+            attempted++;
+        }
+        if (pregunta === '0') {
+            respuesta = 'Correct!';
+        } else if (attempted === attemptsAllowed) {
+            respuesta = 'Sorry!';
+        } 
     } else {
-        while (question !== 'algebra' && question !== 'geometry' && question !== 'algebra 2' ){
+        while (question !== 'algebra' && question !== 'geometry' && question !== 'algebra 2' && attempted !== attemptsAllowed) {
             question = prompt('Be sure to spell the subject as shown in the text: algebra, geometry, algebra 2')
+            attempted++;
         }
         // ASK HOW TO ALLOW CANCEL TO GET OUT OF LOOP AFTER BUTTON CLICK
+        // while (i<3){ if (question !== 'algebra' && question !== 'geometry' && question !== 'algebra 2' c)
+        respuesta = 'Sorry!';
     }
-    return document.write('<p>' + respuesta + '</p>'); 
+    return document.write('<p>' + respuesta + '</p>');
 }
 
 function myFunction() {
     confirm("assume by contradiction the sqrt2 is rational, then sqrt2 can be expressed as p:q where p,q are integers, q neq 0, and gcd(p,q)=1. Now with sqrt2=p:q, squaring both sides will give 2=(p:q)^(2), moving over the q gives 2q^(2)=p^(2). This implies p^(2) is even and thus p is even of the form p=2k, where k is an integer. By substitution then 2q^(2)=p^(2)=(2k)^(2) which gives q^(2)=2k^(2). This implies q^(2) is even and thus q is even. BUT, this contradicts the fact that gcd(p,q)=1, therefore sqrt2 is not rational. QED.*");
 }
 
-function mathQuiz(){
-    var resp = prompt ('Would you like to try a math problem?')
+function mathQuiz() {
+    var resp = prompt('Would you like to try a math problem?')
     var ans;
 
-    if (resp === 'yes' || resp === 'Yes' || resp === 'y' || resp === 'sure'){
+    if (resp === 'yes' || resp === 'Yes' || resp === 'y' || resp === 'sure') {
         ans = 'Prove the irrationality of the square root of 2. (See answer on page to verify)';
     } else {
         ans = 'The answer to the math problem is on the page if you were still curious';
